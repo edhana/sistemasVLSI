a0b5c97e15434ebcac99925b7719f476757579b4
Eduardo S Marques <edhana@gmail.com>
2013-01-11 07:51:00 -0200

    Adicionado condigo para o exercicio 3 de laboratorio Adicionado gitignore para o exercicio 3

--- exercises/lab1_exp_2/mux5x1_tb.vhd
+++ exercises/lab1_exp_2/mux5x1_tb.vhd
@@ -8,7 +8,6 @@ entity mux5x1_tb is
 end mux5x1_tb;
 
 architecture mux5x1_tb_arch of mux5x1_tb is
-  -- constants
   -- signals
   signal a, b, c, d, e: std_logic_vector(2 downto 0);
   signal sel: std_logic_vector(2 downto 0);
@@ -42,7 +41,10 @@ begin
     c <= "001";
     d <= "100";
     e <= "110";
-    -- sel <= "000";
+    -- if I let the line below uncommented I would be modifyng the same signal
+    -- in diferent modules an this could lead to an error in simulation
+    -- sel <= "000"; 
+    
     WAIT; -- suspends the process forever, which means this process is only executed once
   end process init;
 
@@ -50,7 +52,7 @@ begin
   always : process
     variable i : INTEGER := 0;
   begin
-    case i is
+    case i is -- kind of state machine
       when 0 =>
         sel <= "000";
         i := 1;
@@ -60,9 +62,9 @@ begin
       when 2 =>
         sel <= "010";
         i := 3;
-		when 3 => 
-			sel <= "011";
-			i := 4;
+		  when 3 => 
+        sel <= "011";
+        i := 4;
       when others => 
         sel <= "100";
         i := 0;
--- exercises/lab1_exp_3/seteseg.vhd
+++ exercises/lab1_exp_3/seteseg.vhd
@@ -1,5 +1,36 @@
--- Lab 1 exercise 3
 -- seteseg digital display test
+-- Lab 1 exercise 3
 -- Eduardo Marques
 -- 10/01/2013
+
+-- Letters chart
+--             6543210
+-- letter F = "1110001"
+-- letter A = "1110111"
+-- letter C = "0111001"
+-- letter I = "0000110"
+-- letter L = "0111000"
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity seteseg is
+  port(
+      codigo : in std_logic_vector(2 downto 0);
+      segs : out std_logic_vector(6 downto 0)
+    );
+end seteseg;
+
+architecture behaviour of seteseg is
+  signal seg_tmp : std_logic_vector(6 downto 0);
+begin
+  seteseg_selection: with codigo select
+  seg_tmp <= "1110001" when "000", -- F
+        "1110111" when "001",      -- A
+        "0111001" when "011",      -- C
+        "0000110" when "111",      -- I
+        "0111000" when "110",      -- L
+        "0000000" when others; -- show the point
+  segs <= seg_tmp;
+end behaviour;
 
